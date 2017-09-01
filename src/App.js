6cf8cc3ad545e282c19d84960bd5357d3617b31f
a_tretyakov <artem.tretyakov.91@inbox.ru>
2017-09-01 18:11:38 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header';
 
-const menu = [
-    {
-        link:'/articles',
-        label: 'Articles'
-    },
-    {
-        link:'/contacts',
-        label: 'Contacts'
-    },
-    {
-        link:'/posts',
-        label: 'Posts'
-    }
-];
+import './App.css';
+
+import RegistrationForm from './RegistrationForm';
 
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Header items={menu} />
+            <div className="container">
+                <RegistrationForm />
             </div>
         );
     }
--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
-
+import './RegistrationForm.css';
 
 
 class RegistrationForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: ''
+        };
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('form is submitted. This is value:', this.state.email);
+    }
+
+
+    handleEmailChange(event) {
+        console.log('email was changed', event.target.value);
+        this.setState({email:event.target.value})
+    }
+
+
+
     render() {
         return (
             (
-                <div>
-                    Form
-                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="E-mail"
+                        value={this.state.email}
+                        onChange={this.handleEmailChange}
+                        className="emailField"
+                    />
+                    <button className="submitbBtn">Save</button>
+                </form>
             )
         )
     }
